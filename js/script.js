2f255d59d445040ad56f1a4d339d49ce53d37eaf
Aleksandr Mingalev <amingalev@gmail.com>
2023-04-10 21:51:07 +0200

    feat: add audio timer

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ let isPlay = false;
 
 const actorPlayer = document.querySelector('.actor__player');
 const playBtn = document.querySelector('.play__icon')
+let audioCurTime = document.querySelector('.sample__duration');
 const audio = new Audio();
+// let audioCurTime;
+
 
 playBtn.addEventListener('click', ()=> {
+  audio.addEventListener("timeupdate", updateProgress);
   if (!isPlay) {playAudio()}
   else {pauseAudio()}
-  playBtn.classList.toggle('pause')
   });
 
 
@@ -16,11 +19,31 @@ function playAudio() {
   audio.currentTime = 0;
   audio.play();
   isPlay = true;
-  playBtn.style.backgroundImage = "url('./img/png/pause.png')"
+  toggleBtnIcon();
 }
 
 function pauseAudio() {
   audio.pause();
   isPlay = false;
-  playBtn.style.backgroundImage = "url('./img/png/play.png')"
+  toggleBtnIcon(); }
+
+function toggleBtnIcon() {
+  if (isPlay == false) { playBtn.style.backgroundImage = "url('./img/png/play.png')"}
+  if (isPlay == true) {  playBtn.style.backgroundImage = "url('./img/png/pause.png')"}
 }
+
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.target;
+  // const progressPercent = (currentTime / duration) * 100;
+  // progress.style.width = `${progressPercent}%`;
+  let zeros = '00:0',
+      seconds;
+  seconds = Math.floor(currentTime);
+  if (seconds >= 10) {zeros = '00:'};
+  if (seconds > 59) {zeros = '01:0'; seconds -= 60;};
+  if (seconds > 69) {zeros = '01:';};
+  audioCurTime.textContent = `${zeros}${seconds}`;
+  if (currentTime == duration) { pauseAudio(); };
+}
+
